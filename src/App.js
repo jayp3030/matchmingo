bb757a366e98f0187f45108c0cf17e5e08db39f5
jay <jayp33233@gmail.com>
2023-02-18 15:40:17 +0530

    landing page-1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+/* eslint-disable react/jsx-pascal-case */
 import logo from './logo.svg';
 import './App.css';
 import Landing_part02 from './components/Landing_part02';
+import Landing_Part01 from './components/Landing_Part01';
 
 function App() {
   return (
     <>
-    <Landing_part02/>
+    <Landing_Part01 />
+    {/* <Landing_part02/> */}
     </>
   );
 }
