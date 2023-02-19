8cb81326980519cbd6ff5ab1be534c93bf2f154a
Mayank <mayanksheladiya49@gmail.com>
2023-02-19 15:35:09 +0530

    motion gradient animation added

--- src/components/SidePresentation.js
+++ src/components/SidePresentation.js
@@ -1,20 +1,32 @@
 import React from "react";
 
-const SidePresentation = () =>{
-return (
+const SidePresentation = () => {
+  return (
     <>
-    <div className="Side_presentation">
-        <div className="side_presenation_container fcc">
-            <div className="blurer fcc">
-                <div className="logo_div_login fcc">
-                    <p>Match</p>
-                    <p>Mingo</p>
-                </div>
+      <div className="Side_presentation">
+        <div className="side_presenation_container fcc" id="gradient">
+          <div className="blurer fcc" id="blurer">
+            <div className="logo_div_login fcc">
+              <p>Match</p>
+              <p>Mingo</p>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-)
-}
+  );
+};
 
 export default SidePresentation;
+
+setInterval(() => {
+  var a = document.getElementById("blurer");
+  var x = Math.random() * 100;
+  var y = Math.random() * 100;
+
+  var z = x + "px," + y + "px";
+  console.log(z);
+  a.style.backgroundPositionX =x+"px"; 
+  a.style.backgroundPositionY =y+"px"; 
+  a.style.transitionDuration = "1s";
+}, 500);
