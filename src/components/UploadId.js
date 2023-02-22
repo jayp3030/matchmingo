8a43bf0927328f2047593babec0acdcddb163e7c
jay <jayp33233@gmail.com>
2023-02-22 20:07:19 +0530

    upload photo updated

---
+++ src/components/UploadId.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ImageUploading from "react-images-uploading";
+
+export default function UploadId() {
+
+    const Navigate = useNavigate();
+
+  document.addEventListener('mousemove', function(event) { 
+    var rect = document.getElementById('testDiv').getBoundingClientRect(); 
+    var x = event.clientX - rect.left; 
+    var y = event.clientY - rect.top; 
+
+     
+    if(x>0 && x<320 && y>0 && y<40 ){
+        document.getElementById("circle").style.display = "block" 
+        document.getElementById("circle").style.top = y + "px";
+        document.getElementById("circle").style.left = x + "px";
+    }
+      else{
+        document.getElementById("circle").style.display = "none" 
+      }
+      
+  }); 
+  const blastCircle=(e)=>{
+    e.preventDefault()
+    document.getElementById("circle").style.backgroundColor = "#2EC4B6"
+
+    setTimeout(() => {
+      Navigate("/verified")
+    }, 1000);
+    
+    document.getElementById("circle").style.transform = "scale(100)"
+    document.getElementById("circle").style.transition = "all 1s ease-out"
+    document.getElementById("sidebar_wrapper").style.opacity = 0 
+    document.getElementById("matchmingoText").style.opacity = 0 
+    
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+
+  const handlebackwardSlide = (e) => {
+    e.preventDefault();
+    document.getElementById("profile_setup").style.transform =
+      "translateX(-500vw)";
+  };
+
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 20;
+  const onChange = (imageList, addUpdateIndex) => {
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+  return (
+    <>
+      <>
+        <div className="outer_signup" id="outer_signup">
+          <div className="col1"></div>
+          <div className="col2">
+            <div className="upper">
+              <h2>Upload Your ID</h2>
+            </div>
+
+            <ImageUploading
+              multiple
+              value={images}
+              onChange={onChange}
+              maxNumber={maxNumber}
+              dataURLKey="data_url"
+              acceptType={["jpg", "png"]}
+            >
+              {({
+                imageList,
+                onImageUpload,
+                onImageRemoveAll,
+                onImageUpdate,
+                onImageRemove,
+                isDragging,
+                dragProps,
+              }) => (
+                <div
+                  className="photo_section"
+                  style={isDragging ? { color: "var(--light)" } : null}
+                  onClick={onImageUpload}
+                  {...dragProps}
+                >
+                  <i class="fa-solid fa-image"></i>
+                  <p>Click or Drop here</p>
+                </div>
+              )}
+            </ImageUploading>
+
+            {/* <div className="suggestions">
+              <p>
+                <span> &#129488; </span> Minimum 2 Photos
+              </p>
+              <p>
+                <span>&#129331; </span> Selfies Are Good
+              </p>
+              <p>
+                <span>&#10060;</span> Avoid Group Photos
+              </p>
+            </div> */}
+            <div className="middle">
+              <button className="btn" id="testDiv" onClick={handleSubmit} >
+                <button
+                  className="circle"
+                  id="circle"
+                  onClick={blastCircle}
+                ></button>
+                <span>Upload</span>
+              </button>
+              <button className="btn_back" onClick={handlebackwardSlide}>
+                Back
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    </>
+  );
+}
--- src/components/Uploadphoto.js
+++ src/components/Uploadphoto.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ImageUploading from "react-images-uploading";
 
 export default function Uploadphoto() {
+  const buttonToggle = () => {
+    if (images.length < 2) {
+      document.getElementById("uploadPageBtn").disabled = true;
+    } else {
+      document.getElementById("uploadPageBtn").disabled = false;
+    }
+  };
 
-    const handlebackwardSlide = (e)=>{
-        e.preventDefault()
-        document.getElementById("profile_setup").style.transform = "translateX(-400vw)"
-      }
+  const handleSlide = (e) => {
+    e.preventDefault();
+    document.getElementById("profile_setup").style.transform =
+      "translateX(-600vw)";
+  };
+
+  const handlebackwardSlide = (e) => {
+    e.preventDefault();
+    document.getElementById("profile_setup").style.transform =
+      "translateX(-400vw)";
+  };
+
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 20;
+  const onChange = (imageList, addUpdateIndex) => {
+    buttonToggle();
+    setImages(imageList);
+  };
+
+  useEffect(() => {
+    buttonToggle();
+  });
+
   return (
     <>
       <div className="outer_signup" id="outer_signup">
@@ -14,17 +41,61 @@ export default function Uploadphoto() {
           <div className="upper">
             <h2>Upload Some Photos</h2>
           </div>
-          <div className="photo_section">
-          <i class="fa-solid fa-image"></i>
-          </div>
+          {console.log(images)}
+          <ImageUploading
+            multiple
+            value={images}
+            onChange={onChange}
+            maxNumber={maxNumber}
+            dataURLKey="data_url"
+            acceptType={["jpg", "png"]}
+          >
+            {({
+              imageList,
+              onImageUpload,
+              onImageRemoveAll,
+              onImageUpdate,
+              onImageRemove,
+              isDragging,
+              dragProps,
+            }) => (
+              <div
+                className="photo_section"
+                style={isDragging ? { color: "var(--light)" } : null}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                <div className="image_wrapper">
+                  {imageList.map((image, index) => (
+                    <div key={index} className="image-item">
+                      <img src={image.data_url} alt="" width="130" />
+                    </div>
+                  ))}
+                </div>
+                <i class="fa-solid fa-image"></i>
+                <p>Click or Drop here</p>
+              </div>
+            )}
+          </ImageUploading>
+
           <div className="suggestions">
-             <p><span>	&#129488; </span> Minimum 2 Photos</p>
-             <p><span>&#129331; </span> Selfies Are Good</p>
-             <p><span>&#10060;</span> Avoid Group Photos</p>
+            <p>
+              <span> &#129488; </span> Minimum 2 Photos
+            </p>
+            <p>
+              <span>&#129331; </span> Selfies Are Good
+            </p>
+            <p>
+              <span>&#10060;</span> Avoid Group Photos
+            </p>
           </div>
           <div className="middle">
-            <button className="btn">Upload</button>
-            <button className="btn_back" onClick={handlebackwardSlide}>Back</button>
+            <button className="btn" id="uploadPageBtn" onClick={handleSlide}>
+              Upload
+            </button>
+            <button className="btn_back" onClick={handlebackwardSlide}>
+              Back
+            </button>
           </div>
         </div>
       </div>
