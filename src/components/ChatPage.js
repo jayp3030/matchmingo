f8eb13e6506eb3b9b5a1a821b22d788e0132259f
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-24 17:21:48 +0530

    home page left completed

---
+++ src/components/ChatPage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import img from "../images/landingPage03.jpg"
+
+export default function ChatPage() {
+  return (
+    <div className='chatPage'>
+        <div className='chatPage_upper'>
+            <div className='chatPage_upper_left'>
+            <i class="fa-solid fa-chevron-left"></i><img src={img}></img>
+            </div>
+            <div className='chatPage_upper_right'>
+                Pooja Rana
+            </div>
+        </div>
+        <div className='chatPage_middle'>
+
+        </div>
+        <div className='chatPage_bottom'>
+            <div className='chatPage_bottom_suggestions'>
+                <p>Hello</p>
+                <p>Hello,Baby</p>
+                <p>Bye then</p>
+            </div>
+            <div className='chatPage_bottom_inner'>
+            <i class="fa-solid fa-camera"></i>
+            <input type="text" placeholder='Message...'></input>
+            <i class="fa-solid fa-play"></i>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
