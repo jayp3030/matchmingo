4cb40e3b191514497e399fbe39e039e3cdd3428a
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-27 17:34:49 +0530

    added password reset functionality

--- server/src/controllers/userAuth.controller.js
+++ server/src/controllers/userAuth.controller.js
@@ -1,13 +1,24 @@
 const express = require("express")
 const userAuth = require("../model/userAuth.model")
+const Token = require("../model/token.model")
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const crypto = require("crypto")
+var nodemailer = require('nodemailer');
 
 
 const privateKey = process.env.PRIVATE_KEY
+const clientURL = "http://localhost:3000"
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
 
 // function to create a user 
 async function createUser (req,res){
@@ -55,7 +66,7 @@ async function createUser (req,res){
             if(!user){
                 return res.status(400).json({error:"please enter valid credentials"})
             }
-            const passwordCompare =  bcrypt.compare(password,user.password);
+            const passwordCompare =  await bcrypt.compare(password,user.password);
             if(!passwordCompare){
                 return res.status(400).json({error:"please enter valid credentials"})
             }
@@ -68,10 +79,74 @@ async function createUser (req,res){
             success = true;
             res.json({success,token})
         } catch (error) {
-            console.log(error.message)
             res.status(500).send("internal server error")
         }
     }
+    // function to reset password 
+    const requestPasswordReset = async (req,res) => {
+        const {email} = req.body;
+        const user = await userAuth.findOne({ email });
+      
+        if (!user) throw new Error("User does not exist");
+        let token = await Token.findOne({ userId: user._id });
+        if (token) await token.deleteOne();
+        let resetToken = crypto.randomBytes(32).toString("hex");
+        const hash = await bcrypt.hash(resetToken, Number(10));
+      
+        await new Token({
+          userId: user._id,
+          token: hash,
+          createdAt: Date.now(),
+        }).save();
 
+        var mailOptions = {
+            from: 'matchmingo@gmail.com',
+            to: email,
+            subject: 'Sending Email using Node.js',
+            text:`Reset your password at :${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`
+          };
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                return res.status(400)
+            } else {
+              return res.status(200)
+            }
+          });
+      };
 
-module.exports = {createUser,loginUser}
+      const resetPassword = async (req,res) => {
+        const {userId, token, password} = req.body;
+        let passwordResetToken = await Token.findOne({ userId });
+        if (!passwordResetToken) {
+            return res.status(400).json({success:false,err:"Invalid or expired password reset token"});
+        }
+        const isValid = await bcrypt.compare(token, passwordResetToken.token);
+        if (!isValid) {
+            return res.status(400).json({success:false,err:"Invalid or expired password reset token"});
+        }
+        const hash = await bcrypt.hash(password, Number(10));
+        await userAuth.updateOne(
+          { _id: userId },
+          { $set: { password: hash } },
+          { new: true }
+        );
+        const user = await userAuth.findById({ _id: userId });
+        var mailOptions = {
+            from: 'matchmingo@gmail.com',
+            to: user.email,
+            subject: 'Password Updated successfully',
+            text:`Your password has been updated`
+          };
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+              return res.status(200)
+            }
+          });
+        await passwordResetToken.deleteOne();
+        return res.status(200).json({success:true});
+      };
+
+module.exports = {createUser,loginUser,requestPasswordReset,resetPassword}
--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React,{useState} from "react";
 import obj from '../images/google.png'
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 const LoginForm = () => {
+
+  const Navigate = useNavigate();
+  const host = "http://localhost:8000";
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleForgetPassword=async(e)=>{
+    e.preventDefault();
+    const response = await fetch(`${host}/auth/requestResetPassword`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email:credentials.email
+      }),
+    });
+    const json = await response.json();
+  }
+  const handleLogin=async(e)=>{
+    e.preventDefault();
+    const response = await fetch(`${host}/auth/loginUser`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    const json = await response.json();
+    if(json.success){
+      Navigate("/home")
+      return
+    }
+    else{
+      document.getElementById("loginPageAlert").style.opacity = 1
+    }
+  }
+
+
+  const handleOnChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+
   return (
     <>
       <div className="login_form_div">
@@ -35,21 +78,22 @@ const LoginForm = () => {
             <div className="form_container fcc">
                 <div className="form_part1">
                     <form id="login_form_ff" className="fcc">
-                        <input type="email" placeholder="Enter Your Email">
+                        <input type="email" placeholder="Enter Your Email" name="email" id="email" value={credentials.email} onChange={handleOnChange}>
                         </input>
-                        <input type="password" placeholder="Password">
+                        <input type="password" placeholder="Password" name="password" id="password" value={credentials.password} onChange={handleOnChange}>
                         </input>
+                        <p className="loginPageAlert alert" id="loginPageAlert">Please enter valid credentials</p>
                         <div className="remember_me_login">
                             <div className="re01">
                             <input type="checkbox" id="remember"/>
                         <p className="remember_me">&nbsp;Remeber This Device</p>
                             </div>
                             <div className="re02">
-                            <Link to='/' className="link"><p className="forget_password">Forget Password ?</p></Link>
+                            <button className="link" onClick={handleForgetPassword}><p className="forget_password">Forget Password ?</p></button>
                             </div>
                         </div>
                         <div className="submit_btn_div">
-                        <motion.button className="submit_btn"
+                        <motion.button onClick={handleLogin} className="submit_btn"
                         whileHover={{scale:1.02}}
                         >Login</motion.button>
                         <div className="not_an_member">
