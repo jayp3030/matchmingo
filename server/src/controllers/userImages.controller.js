dbe8a4da919b8bb63751fa7f7f2f8509ea5cb401
jay <jayp33233@gmail.com>
2023-03-03 09:40:58 +0530

    editable profile page..

--- server/src/controllers/userImages.controller.js
+++ server/src/controllers/userImages.controller.js
@@ -1,82 +1,69 @@
-// const fs = require('fs');
-// const path = require('path');
-const upload = require("../middleware/uploadImg.middleware");
 const MongoClient = require("mongodb").MongoClient;
+const upload = require("../middleware/uploadImg.middleware");
 // const GridFSBucket = require("mongodb").GridFSBucket;
 
-// const imageModel = require('../model/userImage.model');
 
-const url = 'mongodb://0.0.0.0:27017';
+const url = "mongodb://0.0.0.0:27017";
 
 const baseUrl = "http://localhost:8000/details/userImages";
 
 const mongoClient = new MongoClient(url);
 
-async function saveUserImages(req , res){
-    // const saveImage = await imageModel.create({
-    //     name : req.body.name,
-    //     img : {
-    //         data : fs.readFileSync(req.file.path),
-    //         contentType : req.file.mimetype,
-    //     }
-    // });
+async function saveUserImages(req, res) {
 
-    // res.status(201).json(saveImage);
+  try {
+    await upload(req, res);
+    console.log(req.file);
 
-    try {
-        await upload(req, res);
-        console.log(req.file);
-    
-        if (req.file === undefined) {
-          return res.send({
-            message: "You must select a file.",
-          });
-        }
-    
-        return res.send({
-          message: "File has been uploaded.",
-        });
-      } catch (error) {
-        console.log(error);
-    
-        return res.send({
-          message: `Error when trying upload image: ${error}`,
-        });
-      }
+    if (req.file === undefined) {
+      return res.send({
+        message: "You must select a file.",
+      });
+    }
+
+    return res.send({
+      message: "File has been uploaded.",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.send({
+      message: `Error when trying upload image: ${error}`,
+    });
+  }
 }
 
-async function getImages (req, res){
-    try {
-      await mongoClient.connect();
-  
-      const database = mongoClient.db('MatchMingo');
-      // const images = database.collection('MMImages.files');
-      const chunks = database.collection('MMImages.chunks');
-  
-      const cursor = chunks.find({});
-  
+async function getImages(req, res) {
+  try {
+    await mongoClient.connect();
+
+    const database = mongoClient.db("MatchMingo");
+    // const images = database.collection('MMImages.files');
+    const chunks = database.collection("MMImages.chunks");
+
+    const cursor = chunks.find({});
+
     //   if ((await cursor.collection.estimatedDocumentCount()) === 0) {
     //     return res.status(500).send({
     //       message: "No files found!",
     //     });
     //   }
-  
-      let fileInfos = [];
-      await cursor.forEach((doc) => {
-        fileInfos.push({
-          data : doc.data
-        });
-      });
-      return res.status(200).send(fileInfos);
-    } catch (error) {
-      return res.status(500).send({
-        message: error.message,
-      });
-    }
-  };
 
+    let fileInfos = [];
+    await cursor.forEach((doc) => {
+      fileInfos.push({
+        data: doc.data,
+      });
+    });
+    return res.status(200).send(fileInfos);
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message,
+    });
+  }
+}
 
 module.exports = {
-    saveUserImages,
-    getImages,
-}
+  saveUserImages,
+  getImages,
+};
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,42 +1,29 @@
-const express = require("express")
-const {saveUserDetails} = require("../controllers/user.controller");
-const { body } = require('express-validator');
-// const multer = require('multer');
+const express = require("express");
+const {
+  saveUserDetails,
+  getUserDetails,
+} = require("../controllers/user.controller");
+const { body } = require("express-validator");
 
-const { saveUserImages , getImages } = require("../controllers/userImages.controller");
+const {
+  saveUserImages,
+  getImages,
+} = require("../controllers/userImages.controller");
 
-const userRouter =  express.Router()
+const userRouter = express.Router();
 
-userRouter.post("/userDetails"
-, body('first_name', 'enter a valid name').isLength({ min: 2 })
-, body('last_name', 'enter a valid last_name').isLength({ min: 2 })
-, saveUserDetails
+
+
+userRouter.post(
+  "/userDetails",
+  body("first_name", "enter a valid name").isLength({ min: 2 }),
+  body("last_name", "enter a valid last_name").isLength({ min: 2 }),
+  saveUserDetails
 );
-
-
-
-// const storage = multer.diskStorage({
-//     destination : (req , file , cb) =>{
-//         cb(null , 'uploads');
-//     },
-
-//     filename : (req , file , cb) =>{
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//         cb(null, file.originalname + '-' + uniqueSuffix);
-//     }
-// })
-
-// const upload = multer({storage : storage});
-
-
-// userRouter.post("/userImages",  upload.single('image'), saveUserImages);
-
-
+userRouter.get("/getUserDetails", getUserDetails);
 userRouter.post("/userImages", saveUserImages);
 userRouter.get("/getImages", getImages);
 
-
-
 module.exports = {
-    userRouter,
-}
+  userRouter,
+};
