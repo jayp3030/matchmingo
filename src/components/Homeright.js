57ff5c150b0cc91a300e47128f560e7a429c89d2
Mayank <mayanksheladiya49@gmail.com>
2023-03-06 23:56:48 +0530

    all swipe gestures for mobile has been built

--- src/components/Homeright.js
+++ src/components/Homeright.js
@@ -1,3 +1,4 @@
+import SwipeListener from 'swipe-listener';
 import React, { useEffect, useRef, useState } from "react";
 import img from "../images/landingPage01.jpg";
 import img2 from "../images/landingPage02.jpg";
@@ -23,8 +24,8 @@ export default function Homeright() {
   var [right, setRight] = useState(true);
   var [l, setL] = useState(5);
   var [loc, setLoc] = useState({
-    x: 0,
-    y: 0,
+    x1: null,
+    y1: null,
   });
   var b={
     "name":"mayank1"
@@ -55,7 +56,7 @@ var [obj, setObj] = useState({
     },
  
   });
-  var reference = useRef(0);
+
   
   function update() {
     
@@ -73,69 +74,113 @@ var [obj, setObj] = useState({
   function expand() {
     var count=0;
     var a = document.getElementById("card_right");
-    count=a.style.display;
-    console.log("width =" + count);
+    count=a.style.width;
+    // console.log("width =" + count);
     // if(count%2!=0 && count!=0){a.style.display = "block";}
     // else{a.style.display = "none";}
-    if(count=='' || count=="none" ){a.style.display = "block";}
-    else{a.style.display = "none";}
-    console.log("button clicked");
+    if(count=='' || count=="0%" ){a.style.width="60%";}//a.style.display = "block";
+    else{a.style.width="0%";} //a.style.display = "none";
+    // console.log("button clicked");
     
-    console.log(count);
+    // console.log(count);
+
     // a.style.width="60% !important";
    
   }
-  var cards=document.getElementById("card");
   
-    window.addEventListener("keyup", (e) => {
+  
+    window.addEventListener("keydown", async (e) => {
       console.log(e);
       var card=document.getElementById("card");
-    
+      var user_name=document.getElementById("userNameAge");
+      var user_branch=document.getElementById("userBranch");
       if (e.key === "ArrowLeft") {
-    //     console.log(card);
-    // card.style.marginTop="-100%";
-    // card.style.transitionDuration="1s";
-    // setPersonalDT([b]);
-    // setTimeout(()=>{card.style.marginTop="0%";},1000)
-    setObj({
-      initial: {
-        opacity: 0,
-        x: "100%",
-      },
-      animate: {
-        opacity: 1,
-        x: "0%",
-        transition: {
-          duration: 1,
-          delay:1,
-          ease: "backInOut",
-          when: "beforeChildren",
-        },
-      },
-      exit: {
-        opacity: 0,
-        x: "-100%",
-        transition: {
-          duration: 3,
-          delay:1,
-          ease: "backInOut",
-          when: "beforeChildren",
-        },
-      },
-    });
-        if (l > 1) {
-          setL(l - 1);
-        }
+        update();
+      }
+      else if (e.key === "ArrowRight") {
+        like();
+      }
+      else if(e.key=="ArrowUp" || e.key=="ArrowDown"){
+        console.log("arrow up or down has been pressed");
+        var count=0;
+        var a = document.getElementById("card_right");
+        count=a.style.width;
+        console.log(a);
+        if(count=='' || count=="0%" ){a.style.width="60%";window.alert(a.style.width);}//a.style.display = "block";
+        else{a.style.width="0%";} //a.style.display = "none";
+        
       }
     });
 
  function super_like(){
+  window.alert("super like has been given");
   console.log("super like given");
  }
-  
+ function like(){
+  window.alert("like has been given");
+  console.log("like was given");
+ }
 
-useEffect(()=>{{console.log(l);}},[l])
-  var abc = document.getElementById("moving_part");
+
+
+ window.onload=()=>{
+  document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+ }
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+           /* right swipe */ 
+           expand();
+          console.log("swipe right");
+          
+        } else {
+          /* left swipe */
+          super_like();
+          console.log("swipe left");
+          
+      }                       
+  } else {
+      if ( yDiff > 0 ) {
+         /* down swipe */
+        update();
+        console.log("swipe down");
+          
+      } else { 
+         /* up swipe */
+        like();
+        console.log("swipe up");
+         
+      }                                                                 
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
 
   return (
     <>
@@ -182,28 +227,7 @@ useEffect(()=>{{console.log(l);}},[l])
             <motion.div
               className="moving_part"
               ref={reference}
-              id="moving_part"
-              onPanStart={(e) => {
-                console.log(e);
-                setLoc({ ...loc, x: e.clientX });
-              }}
-              onPanEnd={(e) => {
-                setLoc({ ...loc, y: e.clientY });
-                var card=document.getElementById("card");
-                if (loc.x - loc.y >= 50) {
-                  expand();
-                  // card.style.marginTop="-200%";
-                  // card.style.transitionDuration="1s";
-                  // setPersonalDT([b]);
-                  // setTimeout(()=>{card.style.marginTop="0%";},1000)
-                  if (l > 1) {
-                    setL((l) => l - 1);
-                  }
-                  else if (loc.X - loc.y <= 50) {
-                    expand(); 
-                  }
-                }
-              }}
+              id={"moving_part"}
             >
             <div className="card_Container" id="card_Container">
             <AnimatePresence mode="wait">
@@ -229,8 +253,8 @@ useEffect(()=>{{console.log(l);}},[l])
                     >
                        {/* <img src={img} alt="" /> */} 
                       <div className="userDetails">
-                        <div className="userNameAge">{event.name}Vedant&nbsp;,&nbsp;20</div>
-                        <div className="userBranch">CE</div>
+                        <div className="userNameAge" id="userNameAge">{event.name}Vedant&nbsp;,&nbsp;20</div>
+                        <div className="userBranch" id="userBranch">CE</div>
                       </div>
                       <div className="icons">
                         <motion.i
@@ -370,3 +394,11 @@ useEffect(()=>{{console.log(l);}},[l])
     </>
   );
 }
+// var abc = document.getElementById("moving_part");
+// console.log(abc);
+// abc.ontouchstart=()=>{
+//   console.log("touch started");
+// }
+// abc.ontouchend=()=>{
+//   console.log("touch end");
+// }
