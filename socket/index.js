074882b0bc5eb3aec85398911d675a9e746d1822
jay <jayp33233@gmail.com>
2023-03-09 19:06:21 +0530

    socket io added to chat

--- socket/index.js
+++ socket/index.js
@@ -1,1 +1,42 @@
-const io = require('socket.io')(8800);
+const io = require('socket.io')(8800 , {
+    cors :{
+        origin : 'http://localhost:3000'
+    }
+});
+
+let activeUser = [];
+
+io.on('connection' , (socket) => {
+
+    // add new user
+    socket.on('new-user-add' , (newUserId) => {
+        // user is not already added 
+        if (! activeUser.some( (user) => user.userId === newUserId)) {
+            activeUser.push({
+                userId : newUserId,
+                socketId : socket.id
+            })
+        }
+        console.log('connected users' , activeUser);
+        io.emit('get-users' , activeUser);
+    })
+
+    //send message
+    socket.on('send-message' , (data) => {
+        const {receiverId} = data;
+        const user = activeUser.find( (user) => user.userId === receiverId);
+        console.log('sending to ', user);
+        console.log('data' , data);
+        if (user) {
+            io.to(user.socketId).emit('receive-message' , data);
+        }
+    })
+
+    socket.on('disconnect' , () => {
+        activeUser = activeUser.filter( (user) => user.socketId !== socket.id)
+        console.log('user disconnected' , activeUser);
+        
+        io.emit('get-users' , activeUser);
+    })
+
+})
