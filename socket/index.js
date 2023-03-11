e9b7818b6052e753c6828e8689b00d38d6343c69
Dhruwang <dhruwangjariwala18@gmail.com>
2023-03-11 23:50:21 +0530

    working on chat

--- socket/index.js
+++ socket/index.js
@@ -11,22 +11,28 @@ io.on('connection' , (socket) => {
     // add new user
     socket.on('new-user-add' , (newUserId) => {
         // user is not already added 
+        // console.log("hello")
+        // console.log({newUserId})
         if (! activeUser.some( (user) => user.userId === newUserId)) {
             activeUser.push({
                 userId : newUserId,
                 socketId : socket.id
             })
         }
-        console.log('connected users :' , activeUser);
+        // console.log('connected users :' , activeUser); 
         io.emit('get-users' , activeUser);
     })
 
     //send message
     socket.on('send-message' , (data) => {
-        const {receiverId} = data;
-        const user = activeUser.find( (user) => user.userId === receiverId);
-        console.log('sending to ', user);
-        console.log('data' , data);
+        const receiverId = data.receiverId
+        console.log(receiverId) 
+        console.log({activeUser})
+        const user = activeUser.find((user) => 
+            user.userId === receiverId);
+        // console.log('sending to ', user); 
+        // console.log('data' , data);
+        console.log(user)
         if (user) {
             io.to(user.socketId).emit('receive-message' , data);
         }
