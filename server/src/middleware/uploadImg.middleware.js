b2b326b18e15babea5b1fe2921071ad98b7b354c
jay <jayp33233@gmail.com>
2023-03-14 01:08:53 +0530

    animation added

--- server/src/middleware/uploadImg.middleware.js
+++ server/src/middleware/uploadImg.middleware.js
@@ -6,19 +6,42 @@ const { GridFsStorage } = require("multer-gridfs-storage");
 var storage = new GridFsStorage({
   url: 'mongodb://0.0.0.0:27017/MatchMingo',
   options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req,file) => {
-    console.log(file)
-    const id = req.query.id;
-    const match = ["image/png", "image/jpeg"];
-    if (match.indexOf(file.mimetype) === -1) {
-      const filename = `${id}-${file.originalname}`;
-      return filename;
-    }
+  // file: (req,file) => {
+  //   console.log(file)
+  //   const id = req.query.id;
+  //   const match = ["image/png", "image/jpeg"];
+  //   if (match.indexOf(file.mimetype) === -1) {
+  //     const filename = `${id}-${file.originalname}`;
+  //     return filename;
+  //   }
 
-    return {
-      bucketName: 'users',
-      filename: `${id}-${file.originalname}`
-    };
+  //   return {
+  //     bucketName: 'users',
+  //     filename: `${id}-${file.originalname}`
+  //   };
+  // }
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      // read the image file as a buffer
+      const imageBuffer = file.buffer;
+      
+      // compress the image using sharp
+      sharp(imageBuffer)
+        .jpeg({ quality: 50 })
+        .toBuffer((err, compressedImageBuffer) => {
+          if (err) {
+            return reject(err);
+          }
+          
+          // create a new file in GridFS with the compressed image buffer
+          const filename = id + file.originalname;
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'users'
+          };
+          resolve(fileInfo);
+        });
+    });
   }
 });
 
